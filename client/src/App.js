99c68cd6c64fee68bd3cc7a4ff95eef7dc57723e
Iman Ahsan <iman.ahsan10@gmail.com>
2024-04-25 00:01:19 -0400

    going well

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
-import {Container, AppBar, Typography, Grow, Grid, dialogContentClasses} from '@mui/material';
+import {Container, AppBar, Typography, Grow, Grid} from '@mui/material';
 
 import memories from './images/dina.jpeg';
 
 import Posts from "./components/Posts/posts";
 import Form from "./components/Form/form";
+import useStyles from './styles';
+
 
 const App = () => {
+  const classes = useStyles( );
+
   return (
     <Container maxwidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Memories</Typography>
-        <img src={memories} alt="memories" height="1000"></img>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="memories" height="1000"></img>
       </AppBar>
       <Grow in>
         <Container>
